2478fea669aa93d8998e5ebef661dcb6063206f4
MAXIMILIANO GARBARINO <47433817@ort>
2023-03-23 15:21:13 -0300

    TP1 bis correccion post clase teorica

--- Program.cs
+++ Program.cs
@@ -2,16 +2,15 @@
 int menu;
 bool condicion;
 condicion = true;
-
 Dictionary<string,int> dicCursos = new Dictionary<string,int>();
 do{
     menu = ingresarInt("Ingrese La opcion: 1.Ingrese los importes de un curso    2. Ver estadisticas    otro número. Terminar");
     switch (menu){
         case 1: 
-            solicitarCurso(ref dicCursos);
+            solicitarCurso();
             break;
         case 2:
-            if (dicCursos.Count > 0) verEstadisticas(dicCursos);
+            if (dicCursos.Count > 0) verEstadisticas();
             else Console.WriteLine("Error. Aún no hay estadísticas");
             break;
         default:
@@ -20,13 +19,14 @@ do{
     
 }while(menu == 1 || menu == 2);
 
-void solicitarCurso(ref Dictionary<string,int> dicCursos){
+void solicitarCurso(){
     string nombreCurso;
    int cantAlumnos;
    int acumAlumno = 0;
     nombreCurso = ingresarString("Ingrese el nombre del curso, o 'fin' para finalizar el ingreso.");
+    nombreCurso = validarNombreCurso(nombreCurso);
     while (nombreCurso.ToLower() != "fin") {
-       
+        
         cantAlumnos = ingresarInt("Ingrese la cantidad de alumnos de ese curso");
         for (int i = 1; i <= cantAlumnos; i++)
         {
@@ -36,17 +36,20 @@ void solicitarCurso(ref Dictionary<string,int> dicCursos){
         dicCursos.Add(nombreCurso,acumAlumno);
         acumAlumno = 0;
         nombreCurso = ingresarString("Ingrese el nombre del curso, o 'fin' para finalizar el ingreso.");
+        nombreCurso = validarNombreCurso(nombreCurso);
     }
 }
+
 int ingresarInt(string texto) {
-    int devolver;
+    int devolver = -1;
+    bool valid;
     Console.WriteLine(texto);
     do {
-        devolver = int.Parse(Console.ReadLine());
-        if (devolver < 0) {
+        valid = int.TryParse(Console.ReadLine(), out devolver);
+        if (devolver <= 0 || !valid) {
             Console.WriteLine("error. ingrese nuevamente");
     }
-    } while (devolver <= 0);
+    } while (devolver <= 0 || !valid);
     return devolver;
 }
 
@@ -62,26 +65,35 @@ string ingresarString(string mensaje) {
     return devolver;
 }
 
-void verEstadisticas(Dictionary<string,int> dicCursos) {
+void verEstadisticas() {
     int cursoQueMasPuso = 0;
     string nombreCursoQueMAsPuso = "";
     int acumPlataTodos = 0;
-    int CantCursos = 0;
+    
 foreach (string clave in dicCursos.Keys)
 {
     cursoQueMasPuso = numMayor(cursoQueMasPuso, dicCursos[clave]);
     if (cursoQueMasPuso == dicCursos[clave]) nombreCursoQueMAsPuso = clave;
     acumPlataTodos += dicCursos[clave];
-    CantCursos++;
 }
 
 Console.WriteLine("El curso que más plata puso es " + nombreCursoQueMAsPuso + ", que puso $" + cursoQueMasPuso);
-Console.WriteLine("El promedio de plata de todos los cursos es $" + acumPlataTodos / CantCursos);
+Console.WriteLine("El promedio de plata de todos los cursos es $" + acumPlataTodos / dicCursos.Count);
 Console.WriteLine("La recaudación total de todos los cursos es de $" + acumPlataTodos);
-Console.WriteLine("La cantidad de cursos que participan del regalo es/son " + CantCursos);
+Console.WriteLine("La cantidad de cursos que participan del regalo es/son " + dicCursos.Count);
 }
 
 int numMayor(int num1, int num2) {
     if (num1 < num2) return num2;
     else return num1;
+}
+
+string validarNombreCurso(string nombreCurso) {
+    if (dicCursos.ContainsKey(nombreCurso)) {
+        do {
+            nombreCurso = ingresarString("Ese curso ya existe. Ingrese otro");
+        } while (dicCursos.ContainsKey(nombreCurso));
+       
+    }
+     return nombreCurso;
 }
